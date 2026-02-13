2fc815b5337251706d534aa3929b73a3954f2922
Juan Pablo Mejia <jpmm132006.4@gmail.com>
2026-02-12 18:16:16 -0600

    Funcion modificada

--- main.jl
+++ main.jl
@@ -2,3 +2,10 @@ println("Hola Mundo")
 function suma(a, b)
     return a + b
 end
+
+
+function saludo_personalizado(nombre::String)
+    println("¡Hola, $(nombre)! Bienvenido al repositorio de Julia.")
+end
+
+saludo_personalizado("Compañero")
